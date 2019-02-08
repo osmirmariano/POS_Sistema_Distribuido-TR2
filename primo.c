e8c21da1f66112313678c8837119ea5a46a23574
osmirmariano <osmirmariano@uft.edu.br>
2019-02-08 09:26:42 -0300

    Nova versão do código

--- primo.c
+++ primo.c
@@ -3,17 +3,14 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define TAM 30
+#define TAM 29
 #define RANK_MESTRE 0
 #define TAG_OPERACOES 50
 
 
 int main(int argc, char** argv){
-    int primos[TAM] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 26, 28, 29, 30};
+    int primos[TAM] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 26, 28, 29, 30};
     int x;
-    // for(x = 2; x < TAM; x++){
-    //     primos[x] = x;
-    // }
 
     //Inicialização do MPI
     MPI_Init(NULL, NULL);
@@ -30,40 +27,43 @@ int main(int argc, char** argv){
 	char nome_processo[MPI_MAX_PROCESSOR_NAME];
 	MPI_Get_processor_name(nome_processo, &tam_processo);
 
-    // primos = malloc(TAM*sizeof(int));
-    
     int response[TAM];
+    
     //Condição para identifica o processo do mestre
     if(rank_processo == RANK_MESTRE){
         int x;
+
+        
+
         //Para enviar a operação para os escravos
         for(x = 1; x < num_processo; x++){
-            MPI_Send(&primos[0], TAM, MPI_INT, x, TAG_OPERACOES, MPI_COMM_WORLD);
+            if(x == 1){
+                MPI_Send(primos, TAM, MPI_INT, x, TAG_OPERACOES, MPI_COMM_WORLD);
+            }
         }
         printf("Processo mestre executando em: %s \n", nome_processo);
 
         //Para receber o resultado processado de cada um dos escravos
         for(x = 1; x < num_processo; x++){
-            MPI_Recv(&response[0], TAM, MPI_INT, x, TAG_OPERACOES, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            for(int y = 0; y < 10; y++){
-                printf("Resultado: %d: %d \n", response[y]);
+            MPI_Recv(response, TAM, MPI_INT, x, TAG_OPERACOES, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            for(int y = 0; y < TAM; y++){
+                printf("Resultado: %d \n", response[y]);
             }
         }
     }
     else{
         MPI_Recv(response, TAM, MPI_INT, RANK_MESTRE, TAG_OPERACOES, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        for(int x = 0; x < 10; x++){
+        for(int x = 0; x < TAM; x++){
             printf("Processo escravo %d executando em %s e recebeu %d \n", rank_processo, nome_processo, primos[x]);
         }
 
-        printf("RESPONSE: %d ", rank_processo);
         // Escravo 1 calcula o primeiro bloco
         if(rank_processo == 1){
             response[0] = 1;
-            for(int x = 0; x < 9; x++){
-                if(primos[x] % 2 == 0){
+            for(int x = 2; x < 9; x++){
+                if(primos[x] % x == 0){
                     response[x] = primos[x];
-                    printf("RESPONSE 1: %d\n\n", response[x]);
+                    printf("\nRESPONSE 1: %d\n\n", response[x]);
                 }
             }
         }
@@ -74,7 +74,7 @@ int main(int argc, char** argv){
             for(int x = 10; x < 19; x++){
                 if(primos[x] % x == 0){
                     response[x] = primos[x];
-                    printf("RESPONSE 2: %d\n\n", response[x]);
+                    printf("\nRESPONSE 2: %d\n\n", response[x]);
                 }
             }
         }
@@ -83,10 +83,10 @@ int main(int argc, char** argv){
         else if(rank_processo == 2){
             response[0] = 3;
             
-            for(int x = 20; x < 30; x++){
+            for(int x = 20; x < 29; x++){
                 if(primos[x] % x == 0){
                     response[x] = primos[x];
-                    printf("RESPONSE 3: %d\n\n", response[x]);
+                    printf("\nRESPONSE 3: %d\n\n", response[x]);
                 }
             }
         }
